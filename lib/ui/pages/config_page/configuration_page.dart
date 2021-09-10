d4b65e6dd644ae65fa1b693adf50943951ea4ee7
Aristocrat <crataristo4@gmail.com>
2021-09-10 15:08:21 +0000

    added configuration_page.dart

--- lib/ui/pages/config_page/configuration_page.dart
+++ lib/ui/pages/config_page/configuration_page.dart
@@ -1,5 +1,9 @@
+import 'package:bus_io/ui/pages/main_page/main_page.dart';
+import 'package:bus_io/ui/pages/sign_up_page/sign_up_page.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+///checks if the user is already logs in
 class ConfigPage extends StatefulWidget {
   static const routeName = '/configPage';
 
@@ -10,8 +14,21 @@ class ConfigPage extends StatefulWidget {
 }
 
 class _ConfigPageState extends State<ConfigPage> {
+  bool isLoggedIn = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return isLoggedIn ? MainPage() : SignupPage();
+  }
+
+  @override
+  void initState() {
+    getCurrentUser();
+    super.initState();
+  }
+
+  getCurrentUser() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    isLoggedIn = prefs.getBool("isLoggedIn")!;
   }
 }
