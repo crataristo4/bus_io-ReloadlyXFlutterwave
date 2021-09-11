6e41c1e96e8b21d2633afbd97529fc9afc14f32b
Aristocrat <crataristo4@gmail.com>
2021-09-11 18:56:01 +0000

    added bottom nav and pages

--- lib/ui/pages/main_page/main_page.dart
+++ lib/ui/pages/main_page/main_page.dart
@@ -1,3 +1,9 @@
+import 'package:bus_io/constansts/dimens.dart';
+import 'package:bus_io/constansts/strings.dart';
+import 'package:bus_io/ui/pages/bookings/bookings_page.dart';
+import 'package:bus_io/ui/pages/home_page/home_page.dart';
+import 'package:bus_io/ui/pages/profile/profile_page.dart';
+import 'package:bus_io/ui/pages/wallet/wallet_page.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -10,8 +16,82 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+  List<Widget> _widgetOptions = <Widget>[
+    HomePage(),
+    BookingsPage(),
+    WalletPage(),
+    ProfilePage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      backgroundColor: Colors.white,
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Container(
+              padding: EdgeInsets.all(eightDp),
+              child: Image.asset(
+                'assets/icons/home.png',
+                width: twentyFourDp,
+              ),
+            ),
+            label: home,
+            tooltip: home,
+          ),
+          BottomNavigationBarItem(
+              icon: Container(
+                padding: EdgeInsets.all(eightDp),
+                child: Image.asset(
+                  'assets/icons/bookings.png',
+                  width: twentyFourDp,
+                ),
+              ),
+              label: bookings,
+              tooltip: bookings),
+          BottomNavigationBarItem(
+            icon: Container(
+              padding: EdgeInsets.all(eightDp),
+              child: Image.asset(
+                'assets/icons/wallet.png',
+                width: twentyFourDp,
+              ),
+            ),
+            label: wallet,
+            tooltip: wallet,
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              padding: EdgeInsets.all(eightDp),
+              child: Image.asset(
+                'assets/icons/profile.png',
+                width: twentyFourDp,
+              ),
+            ),
+            label: profile,
+            tooltip: profile,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Theme.of(context).primaryColor,
+        backgroundColor: Color(0xFFFFFFFF),
+        unselectedItemColor: Color(0xFFAFAFAF),
+        showUnselectedLabels: true,
+        type: BottomNavigationBarType.fixed,
+        onTap: _onItemTapped,
+        elevation: 0,
+      ),
+    );
   }
 }
