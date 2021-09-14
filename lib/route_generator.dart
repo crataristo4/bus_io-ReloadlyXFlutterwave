35647247f78f77c64c1fd1a6952c9dd23063700f
Aristocrat <crataristo4@gmail.com>
2021-09-14 04:24:58 +0000

    modified

--- lib/route_generator.dart
+++ lib/route_generator.dart
@@ -1,8 +1,12 @@
+import 'package:bus_io/ui/pages/add_passenger_details/add_passenger_details.dart';
 import 'package:bus_io/ui/pages/config_page/configuration_page.dart';
 import 'package:bus_io/ui/pages/login_page/login_page.dart';
 import 'package:bus_io/ui/pages/main_page/main_page.dart';
 import 'package:bus_io/ui/pages/onboarding_page/onboarding_page.dart';
+import 'package:bus_io/ui/pages/review_booking/review_booking_details.dart';
 import 'package:bus_io/ui/pages/search_bus/search_bus_page.dart';
+import 'package:bus_io/ui/pages/search_results/search_results_page.dart';
+import 'package:bus_io/ui/pages/select_seat/select_seat.dart';
 import 'package:bus_io/ui/pages/sign_up_page/sign_up_page.dart';
 import 'package:flutter/material.dart';
 
@@ -13,14 +17,36 @@ class RouteGenerator {
     switch (settings.name) {
       case ConfigPage.routeName:
         return MaterialPageRoute(builder: (_) => ConfigPage());
+      //ONBOARDING
       case OnboardingPage.routeName:
         return MaterialPageRoute(builder: (_) => OnboardingPage());
+      //LOGIN
       case LoginPage.routeName:
         return MaterialPageRoute(builder: (_) => LoginPage());
+      //SIGN UP
       case SignupPage.routeName:
         return MaterialPageRoute(builder: (_) => SignupPage());
+      //MAIN PAGE
       case MainPage.routeName:
         return MaterialPageRoute(builder: (_) => MainPage());
+      //REVIEW BOOKING
+      case ReviewBookingDetails.routeName:
+        return MaterialPageRoute(builder: (_) => ReviewBookingDetails());
+//search results
+      case SearchResultsPage.routeName:
+        return MaterialPageRoute(builder: (_) => SearchResultsPage());
+
+      //select seats
+      case SelectSeat.routeName:
+        return MaterialPageRoute(builder: (_) => SelectSeat());
+      //add passenger details
+      case AddPassengerDetails.routeName:
+        final data = args as List<String>;
+        return MaterialPageRoute(
+            builder: (_) => AddPassengerDetails(
+                  seatsSelected: data,
+                ));
+      //SEARCH BUS
       case SearchBus.routeName:
         bool data = args as bool;
         return MaterialPageRoute(
