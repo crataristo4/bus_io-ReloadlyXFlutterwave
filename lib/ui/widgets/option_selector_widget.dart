7c9a681a838f334dbb60fe5b368857ee313a61e6
Aristocrat <crataristo4@gmail.com>
2021-09-19 02:36:46 +0000

    added new

---
+++ lib/ui/widgets/option_selector_widget.dart
@@ -0,0 +1,60 @@
+import 'package:bus_io/constansts/dimens.dart';
+import 'package:flutter/material.dart';
+
+class OptionSelector extends StatefulWidget {
+  final title, icon;
+  final Widget widget;
+  final Color textColor;
+
+  const OptionSelector(
+      {Key? key,
+      required this.title,
+      required this.textColor,
+      required this.icon,
+      required this.widget})
+      : super(key: key);
+
+  @override
+  _OptionSelectorState createState() => _OptionSelectorState();
+}
+
+class _OptionSelectorState extends State<OptionSelector> {
+  @override
+  Widget build(BuildContext context) {
+    //date card
+    return Container(
+      width: 180,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: tenDp),
+            child: Text(
+              widget.title,
+              style: TextStyle(color: widget.textColor, fontSize: fourteenDp),
+            ),
+          ),
+          Card(
+            elevation: 2,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(tenDp),
+            ),
+            child: Container(
+              height: fiftyTwoDp,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(eightDp),
+                    child: Image.asset(widget.icon),
+                  ),
+                  widget.widget
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
