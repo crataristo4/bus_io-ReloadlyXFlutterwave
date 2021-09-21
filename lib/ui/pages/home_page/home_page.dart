094adfbaa01c2ee541780d52c55c61e529070d06
Aristocrat <crataristo4@gmail.com>
2021-09-21 18:58:02 +0000

    modified

--- lib/ui/pages/home_page/home_page.dart
+++ lib/ui/pages/home_page/home_page.dart
@@ -29,12 +29,13 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   TextEditingController noOfPassengerController = TextEditingController();
-  String? today;
+  String? date;
   String? message;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
-    today = dateFormat.format(dateTime);
+    date = dateFormat.format(dateTime);
     greetingMessage();
     super.initState();
   }
@@ -69,252 +70,170 @@ class _HomePageState extends State<HomePage> {
         toolbarHeight: 0,
         backgroundColor: Colors.white,
       ),
-      body: StreamBuilder<Users>(
-          stream: UserService.instance.getUser(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            } else {
-              Users users = Users.fromJson(snapshot.data!.toJson());
+      body: Form(
+        key: _formKey,
+        child: StreamBuilder<Users>(
+            stream: UserService.instance.getUser(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else {
+                Users users = Users.fromJson(snapshot.data!.toJson());
 
-              return SingleChildScrollView(
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: sixteenDp),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: sixteenDp,
-                      ),
-                      Text(
-                        "$message ${users.firstName}",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: twentyFourDp,
-                            color: Colors.black),
-                      ),
-                      SizedBox(
-                        height: fourDp,
-                      ),
-                      Text(
-                        whereAreYouHeaded,
-                        style: TextStyle(
-                            fontSize: fourteenDp, color: Colors.black),
-                      ),
-                      SizedBox(
-                        height: twentyDp,
-                      ),
-                      DestinationCard(
-                        isCard: true,
-                        from: users.from.toString().isEmpty
-                            ? whereAreYouLeavingFrom
-                            : users.from.toString(),
-                        to: users.to.toString().isEmpty
-                            ? whereAreYouGoingTo
-                            : users.to.toString(),
-                        color: Colors.grey,
-                      ),
-                      SizedBox(
-                        height: twentyDp,
-                      ),
-                      Wrap(
-                        direction: Axis.horizontal,
-                        spacing: 0,
-                        children: [
-                          GestureDetector(
-                            onTap: () async {
-                              final selectedDate =
-                                  await ShowAction().selectDate(context);
-                              if (selectedDate == null) return;
-                              dateTime = DateTime(
-                                selectedDate.day,
-                                selectedDate.month,
-                                selectedDate.year,
-                              );
-                              setState(() {
-                                today = dateFormat.format(dateTime);
-                              });
-
-                              print('?? $today');
-                            },
-                            child: OptionSelector(
-                              title: date,
-                              icon: 'assets/icons/calender.png',
-                              widget: Text('$today'),
+                return SingleChildScrollView(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: sixteenDp),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: sixteenDp,
+                        ),
+                        Text(
+                          "$message ${users.firstName}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: twentyFourDp,
+                              color: Colors.black),
+                        ),
+                        SizedBox(
+                          height: fourDp,
+                        ),
+                        Text(
+                          whereAreYouHeaded,
+                          style: TextStyle(
+                              fontSize: fourteenDp, color: Colors.black),
+                        ),
+                        SizedBox(
+                          height: twentyDp,
+                        ),
+                        DestinationCard(
+                          isCard: true,
+                          from: users.from.toString().isEmpty
+                              ? whereAreYouLeavingFrom
+                              : users.from.toString(),
+                          to: users.to.toString().isEmpty
+                              ? whereAreYouGoingTo
+                              : users.to.toString(),
+                          color: Colors.grey,
+                        ),
+                        SizedBox(
+                          height: twentyDp,
+                        ),
+                        Wrap(
+                          direction: Axis.horizontal,
+                          spacing: 0,
+                          children: [
+                            GestureDetector(
+                              onTap: () async {
+                                final selectedDate =
+                                    await ShowAction().selectDate(context);
+                                if (selectedDate == null) return;
+                                date = dateFormat.format(selectedDate);
+                                setState(() {});
+                              },
+                              child: OptionSelector(
+                                title: date,
+                                icon: 'assets/icons/calender.png',
+                                widget: Text('$date'),
+                                textColor: Colors.black,
+                              ),
+                            ),
+                            OptionSelector(
+                              title: noOfPassengers,
+                              icon: 'assets/icons/person.png',
+                              widget: buildNoOfPassengerInput(),
                               textColor: Colors.black,
                             ),
-                          ),
-                          OptionSelector(
-                            title: noOfPassengers,
-                            icon: 'assets/icons/person.png',
-                            widget: buildNoOfPassengerInput(),
-                            textColor: Colors.black,
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: twentyDp,
-                      ),
-                      ButtonWidget(
-                          buttonName: findBuses,
-                          onButtonTapped: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => BusResultsPage()));
-                          }),
-                      SizedBox(
-                        height: twentyDp,
-                      ),
-                      Divider(),
-                      SizedBox(
-                        height: tenDp,
-                      ),
-                      Text(
-                        quickBookings,
-                        style: TextStyle(
-                            fontSize: twentyDp, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        height: sixteenDp,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          buildQuickBookings(lagosToAccra, '09/10/2021'),
-                          buildQuickBookings(lagosToBenin, '09/10/2021'),
-                        ],
-                      ),
-                      SizedBox(
-                        height: sixteenDp,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          buildQuickBookings(accraToLome, '09/10/2021'),
-                          buildQuickBookings(lomeToYaounde, '09/10/2021'),
-                        ],
-                      ),
-                      SizedBox(
-                        height: sixteenDp,
-                      ),
-                      buildLoyaltyCard(),
-                      SizedBox(
-                        height: sixteenDp,
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            }
-          }),
-    );
-  }
-
-/*
-  //destination card
-  Widget buildDestinationCard() {
-    return Card(
-      elevation: 2,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(tenDp)),
-      child: Container(
-        height: 140,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                Container(
-                    padding: EdgeInsets.symmetric(
-                        vertical: twentyDp, horizontal: twentyDp),
-                    child: Column(
-                      children: [
-                        Image.asset('assets/icons/greencircle.png'),
-                        Image.asset('assets/icons/line.png'),
-                        Image.asset('assets/icons/greencircle.png')
+                          ],
+                        ),
+                        SizedBox(
+                          height: twentyDp,
+                        ),
+                        ButtonWidget(
+                            buttonName: findBuses,
+                            onButtonTapped: () {
+                              //if validations then proceed to search buses
+                              //check if user has selected destinations
+                              if (users.from!.isNotEmpty &&
+                                  users.to!.isNotEmpty) {
+                                //check if user has input number of passengers
+                                if (_formKey.currentState!.validate()) {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (context) => BusResultsPage(
+                                            numberOfPassengers: int.parse(
+                                                noOfPassengerController.text),
+                                            to: users.to,
+                                            date: date,
+                                            from: users.from,
+                                          )));
+                                } else {
+                                  ShowAction().showToast(
+                                      "Please enter a valid number for passenger",
+                                      Colors.red);
+                                }
+                              }
+                            }),
+                        SizedBox(
+                          height: twentyDp,
+                        ),
+                        Divider(),
+                        SizedBox(
+                          height: tenDp,
+                        ),
+                        Text(
+                          quickBookings,
+                          style: TextStyle(
+                              fontSize: twentyDp, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: sixteenDp,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            buildQuickBookings(lagosToAccra, '09/10/2021'),
+                            buildQuickBookings(lagosToBenin, '09/10/2021'),
+                          ],
+                        ),
+                        SizedBox(
+                          height: sixteenDp,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            buildQuickBookings(accraToLome, '09/10/2021'),
+                            buildQuickBookings(lomeToYaounde, '09/10/2021'),
+                          ],
+                        ),
+                        SizedBox(
+                          height: sixteenDp,
+                        ),
+                        buildLoyaltyCard(),
+                        SizedBox(
+                          height: sixteenDp,
+                        ),
                       ],
-                    )),
-                Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: twentyDp),
-                        child: Text(
-                          from,
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: twelveDp),
-                        child: GestureDetector(
-                          onTap: () {
-                            //push to search page
-                            Navigator.of(context).pushNamed(SearchBus.routeName,
-                                arguments: false);
-                          },
-                          child: Text(
-                            HomePage.fromLocation,
-                            style: TextStyle(
-                                color: Colors.grey,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: twelveDp),
-                        child: Text(
-                          to,
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: twelveDp),
-                        child: GestureDetector(
-                          onTap: () {
-                            //push to search page
-                            Navigator.of(context).pushNamed(SearchBus.routeName,
-                                arguments: true);
-                          },
-                          child: Text(
-                            HomePage.toLocation,
-                            style: TextStyle(
-                                color: Colors.grey,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Container(
-              child: Image.asset('assets/icons/arrow.png'),
-              padding: EdgeInsets.only(right: thirtyDp),
-            )
-          ],
-        ),
+                );
+              }
+            }),
       ),
     );
   }
-*/
 
   Widget buildNoOfPassengerInput() {
     return Container(
       width: hundredDp,
       child: TextFormField(
-          keyboardType: TextInputType.numberWithOptions(decimal: false),
+          keyboardType: TextInputType.number,
           textAlign: TextAlign.center,
           controller: noOfPassengerController,
-          validator: (value) => value!.trim().isNotEmpty || int.parse(value) > 0
-              ? null
-              : requireD,
+          validator: (value) => value!.trim().isNotEmpty ? null : requireD,
           decoration: InputDecoration(
             hintText: "1",
             fillColor: CustomColors.lightTeal.withOpacity(0.4),
