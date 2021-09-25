a46f6daa06fdda9a60b24700aec4d4507341737a
Aristocrat <crataristo4@gmail.com>
2021-09-25 10:18:03 +0000

    added reloadly_api.dart

---
+++ lib/api/reloadly_api.dart
@@ -0,0 +1,91 @@
+import 'dart:convert';
+
+import 'package:bus_io/constansts/strings.dart';
+import 'package:bus_io/model/gift_card.dart';
+import 'package:bus_io/model/token.dart';
+import 'package:http/http.dart' as http;
+
+class ReloadLyApiService {
+  static ReloadLyApiService? _instance;
+  final clientId = reloadLyClientId;
+  final secretId = reloadLySecretId;
+
+  ReloadLyApiService._();
+
+  static ReloadLyApiService get instance {
+    return _instance == null ? _instance = ReloadLyApiService._() : _instance!;
+  }
+
+  //top ups
+  //get token
+  Future<String> fetchTopUpToken() async {
+    var options = {
+      "client_id": clientId,
+      "client_secret": secretId,
+      "grant_type": "client_credentials",
+      "audience": reloadLyTopUpAudience
+    };
+
+    final getToken = await http.post(Uri.parse(reloadLyTokenUrl),
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: json.encode(options));
+
+    return getToken.body;
+  }
+
+  Future<String> fetchGiftCardToken() async {
+    var options = {
+      "client_id": clientId,
+      "client_secret": secretId,
+      "grant_type": "client_credentials",
+      "audience": reloadLyGiftCardAudience
+    };
+
+    final getToken = await http.post(Uri.parse(reloadLyTokenUrl),
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: json.encode(options));
+
+    return getToken.body;
+  }
+
+  //gift cards
+  Future<List<Content>> fetchGiftCards() async {
+    var tokenOption = {
+      "client_id": clientId,
+      "client_secret": secretId,
+      "grant_type": "client_credentials",
+      "audience": reloadLyGiftCardAudience
+    };
+
+    //get token
+    final token = await http
+        .post(Uri.parse(reloadLyTokenUrl),
+            headers: {
+              'Content-type': 'application/json',
+            },
+            body: json.encode(tokenOption))
+        .then((value) {
+      return value.body;
+    });
+
+    Token getToken = tokenFromJson(token);
+    print('token is ${getToken.accessToken}');
+
+    //get all products
+    final response = await http.get(
+      Uri.parse(reloadLyGiftCardProducts),
+      headers: {
+        'Authorization': 'Bearer ${getToken.accessToken}',
+        'Accept': 'application/com.reloadly.giftcards-v1+json',
+      },
+    );
+
+    final contents = GiftCard.fromJson(json.decode(response.body));
+
+    return contents.content;
+  }
+}
